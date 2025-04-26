dbbc79530017b03f9ce5e3800d7d6f60099afc2a
0274074 <142900020+0274074@users.noreply.github.com>
2025-04-25 21:00:09 -0400

    Update SignInSheet.java
    signing -in, and spell check correction

--- SignInSheet.java
+++ SignInSheet.java
@@ -4,7 +4,7 @@
  * File Name: SignInSheet.java
  * Last Update: 4/22/25 by Lillian H.
  * Last Update: 4/22/25 by James F.
- * Program Description: Program allows a user to enter their name into an array and prints out a confirmation message when ran.
+ * Program Description: The Program allows a user to enter their name into an array and prints out a confirmation message when run.
  */
 
 /*
@@ -24,15 +24,15 @@ Tiffany W. - 4/22/2025 - Added Tiffany as the fourth element in the nameList arr
         // Array to store our names
         String[] nameList = new String[6];
         
-        // Enter your name in the below list on the first available index you see. See my name for an example.
+        // Enter your name in the list below on the first available index you see. See my name for an example.
         nameList[0] = "Seth";
         nameList[1] = "Lillian";
         nameList[2] = "James";
         nameList[3] = "Tiffany";
-        nameList[4] = "_Whitney";
+        nameList[4] = "Whitney";
         nameList[5] = "_______";
         
-        // Prints out statement showing you've modified the file.
+        // Prints out a statement showing you've modified the file.
         System.out.println();
         for (int i = 0; i <= 5; i++) {
             if (!nameList[i].isEmpty()) System.out.println("Hello, I'm " + nameList[i] + " and I'm able to modify the code!");
